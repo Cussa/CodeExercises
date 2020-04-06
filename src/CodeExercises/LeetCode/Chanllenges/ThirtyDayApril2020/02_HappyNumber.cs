6bf0e4a8f796089e0d094f2a21dbbfeb9c753ddd
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-06 19:48:14 +0200

    feat: LeetCode 30DaysCoding Chanllenge - April 02 - HappyNumber

---
+++ src/CodeExercises/LeetCode/Chanllenges/ThirtyDayApril2020/02_HappyNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    // 2020-04-02: https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3284/
+    public class HappyNumber
+    {
+        public bool Resolve(int n)
+        {
+            var visited = new HashSet<int>();
+            var currentValue = n;
+            
+            while (currentValue != 1)
+            {
+                if (visited.Contains(currentValue))
+                    return false;
+
+                visited.Add(currentValue);
+                var sumResult = 0;
+
+                while (currentValue > 0)
+                {
+                    var currentNumber = currentValue % 10;
+                    currentValue /= 10;
+                    sumResult += (int)Math.Pow(currentNumber, 2);
+                }
+                currentValue = sumResult;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Chanllenges/ThirtyDayApril2020/02_HappyNumberTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class _02_HappyNumberTests
+    {
+        [Test]
+        [TestCase(19, true)]
+        [TestCase(20, false)]
+        [TestCase(2, false)]
+        [TestCase(1, true)]
+        [TestCase(10, true)]
+        [TestCase(24, false)]
+        [TestCase(1111111, true)]
+        public void TestSolution(int input1, bool expectedResult)
+        {
+            var result = new HappyNumber().Resolve(input1);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
