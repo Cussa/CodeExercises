52097fb46b8b24d3f87fddfe30a5cb225c5dd23a
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-07 18:39:58 +0200

    feat: LeetCode 30DaysCoding Chanllenge - April 07 - CountingElements

---
+++ src/CodeExercises/LeetCode/Chanllenges/ThirtyDayApril2020/07_CountingElements.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    // 2020-04-07: https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3289/
+    public class CountingElements
+    {
+        public int Resolve(int[] arr)
+        {
+            var count = 0;
+            var visited = new HashSet<int>();
+            var baseArray = new HashSet<int>();
+            var countedNumbers = new HashSet<int>();
+            foreach (var number in arr)
+            {
+                baseArray.Add(number);
+            }
+
+            foreach (var number in arr)
+            {
+                if (countedNumbers.Contains(number))
+                {
+                    count++;
+                    continue;
+                }
+
+                if (visited.Contains(number))
+                {
+                    continue;
+                }
+
+                if (baseArray.Contains(number + 1))
+                {
+                    count++;
+                    countedNumbers.Add(number);
+                    visited.Add(number);
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Chanllenges/ThirtyDayApril2020/07_CountingElementsTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class _07_CountingElementsTests
+    {
+        [Test]
+        [TestCase(new[] { 1, 2, 3 }, 2)]
+        [TestCase(new[] { 1, 1, 3, 3, 5, 5, 7, 7 }, 0)]
+        [TestCase(new[] { 1, 3, 2, 3, 5, 0 }, 3)]
+        [TestCase(new[] { 1, 1, 2, 2 }, 2)]
+        public void TestSolution(int[] input1, int expectedResult)
+        {
+            var result = new CountingElements().Resolve(input1);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
