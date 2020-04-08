1a6a0a671e380a7b7b52451c7dc4bcebb645cff4
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-08 12:07:49 +0200

    feat: LeetCode 0007 - ReverseInteger

---
+++ src/CodeExercises/LeetCode/0007_ReverseInteger.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeExercises.LeetCode
+{
+    // 7. https://leetcode.com/problems/reverse-integer/
+    public class ReverseInteger
+    {
+        public int Solve(int x)
+        {
+            var result = 0L;
+
+            while (x != 0)
+            {
+                var add = x % 10;
+                result = result * 10 + add;
+                x /= 10;
+            }
+
+            if (result < int.MinValue || result > int.MaxValue)
+                return 0;
+
+            return (int)result;
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/0007_ReverseIntegerTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeExercises.LeetCode;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode
+{
+    public class _0007_ReverseIntegerTests
+    {
+        [Test]
+        [TestCase(123,321)]
+        [TestCase(-123, -321)]
+        [TestCase(120, 21)]
+        [TestCase(1534236469, 0)]
+        public void TestSolution(int number, int expectedResult)
+        {
+            var result = new ReverseInteger().Solve(number);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
