0561d18f3c3ed38daa4e4ee9eee10e26b610f2b2
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-08 16:47:58 +0200

    feat: LeetCode 30DaysCoding Chanllenge - April 06 - GroupAnagrams

---
+++ src/CodeExercises/LeetCode/Chanllenges/ThirtyDayApril2020/06_GroupAnagrams.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    // 2020-04-06: https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3288/
+    // 0049. https://leetcode.com/problems/group-anagrams/
+    public class GroupAnagrams
+    {
+        public IList<IList<string>> Resolve(string[] strs)
+        {
+            var dictionary = new Dictionary<string, IList<string>>();
+
+            foreach (var str in strs)
+            {
+                var m = new int[26];
+                foreach (var current in str.ToCharArray())
+                    m[current - 'a']++;
+
+                var key = string.Join(",", m);
+                if (dictionary.ContainsKey(key))
+                    dictionary[key].Add(str);
+                else
+                    dictionary.Add(key, new List<string> { str });
+            }
+
+            return dictionary.Values.ToList();
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Chanllenges/ThirtyDayApril2020/06_GroupAnagramsTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class _06_GroupAnagramsTests
+    {
+        static readonly object[] sourceObjects = 
+        {
+            new object[]
+            {
+                new[] { "eat", "tea", "tan", "ate", "nat", "bat" },
+                new List<IList<string>>
+                {
+                    new[] {"ate", "eat", "tea"},
+                    new[] {"nat", "tan"},
+                    new[] {"bat"}
+                }
+            }
+        };
+
+        [Test]
+        [TestCaseSource(nameof(sourceObjects))]
+        public void TestSolution(string[] input1, IList<IList<string>> expectedResult)
+        {
+            var result = new GroupAnagrams().Resolve(input1);
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
