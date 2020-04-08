3dca7402ab593a29a77b0bd217e28aea88c6a8ba
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-08 12:23:35 +0200

    feat: LeetCode 30DaysCoding Chanllenge - April 03 - MaximumSubarray

---
+++ src/CodeExercises/LeetCode/Chanllenges/ThirtyDayApril2020/03_MaximumSubarray.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    // 2020-04-03: https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3285/
+    public class MaximumSubarray
+    {
+        public int Resolve(int[] nums)
+        {
+            var sum = 0;
+            var maxSum = nums[0];
+
+            foreach (var number in nums)
+            {
+                sum += number;
+                if (number > sum)
+                    sum = number;
+                if (sum > maxSum)
+                    maxSum = sum;
+            }
+
+            return maxSum;
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Chanllenges/ThirtyDayApril2020/03_MaximumSubarrayTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class _03_MaximumSubarrayTests
+    {
+        [Test]
+        [TestCase(new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }, 6)]
+        [TestCase(new[] { -2, 1, -3, 4, -1, 2 }, 5)]
+        public void TestSolution(int[] input1, int expectedResult)
+        {
+            var result = new MaximumSubarray().Resolve(input1);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
