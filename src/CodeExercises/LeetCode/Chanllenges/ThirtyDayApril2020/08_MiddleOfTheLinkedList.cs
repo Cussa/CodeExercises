2e4c3ed0d03ab6931af8b6f97a0b95420b6a1605
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-08 12:04:55 +0200

    feat: LeetCode 30DaysCoding Chanllenge - April 08 - MiddleOfTheLinkedList

---
+++ src/CodeExercises/LeetCode/Chanllenges/ThirtyDayApril2020/08_MiddleOfTheLinkedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    // 2020-04-08: https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/529/week-2/3290/
+    public class MiddleOfTheLinkedList
+    {
+        public ListNode Resolve(ListNode head)
+        {
+            var totalNodes = 1;
+            var current = head;
+
+            while (current.next != null)
+            {
+                totalNodes++;
+                current = current.next;
+            }
+
+            var middleIndex = (int)Math.Ceiling(totalNodes / 2D);
+            if (totalNodes % 2 == 0)
+                middleIndex++;
+
+            var middleNode = head;
+            for (var i = 1; i < middleIndex; i++)
+                middleNode = middleNode.next;
+
+            return middleNode;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Chanllenges/ThirtyDayApril2020/08_MiddleOfTheLinkedListTests.cs
@@ -0,0 +1,70 @@
+﻿using CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class _08_MiddleOfTheLinkedListTests
+    {
+        [Test]
+        public void TestSolution001()
+        {
+            var input = new MiddleOfTheLinkedList.ListNode(1)
+            {
+                next = new MiddleOfTheLinkedList.ListNode(2)
+                {
+                    next = new MiddleOfTheLinkedList.ListNode(3)
+                    {
+                        next = new MiddleOfTheLinkedList.ListNode(4)
+                        {
+                            next = new MiddleOfTheLinkedList.ListNode(5)
+                        }
+                    }
+                }
+            };
+
+            var expectedResult = new MiddleOfTheLinkedList.ListNode(3)
+            {
+                next = new MiddleOfTheLinkedList.ListNode(4)
+                {
+                    next = new MiddleOfTheLinkedList.ListNode(5)
+                }
+            };
+
+            var result = new MiddleOfTheLinkedList().Resolve(input);
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Test]
+        public void TestSolution002()
+        {
+            var input = new MiddleOfTheLinkedList.ListNode(1)
+            {
+                next = new MiddleOfTheLinkedList.ListNode(2)
+                {
+                    next = new MiddleOfTheLinkedList.ListNode(3)
+                    {
+                        next = new MiddleOfTheLinkedList.ListNode(4)
+                        {
+                            next = new MiddleOfTheLinkedList.ListNode(5)
+                            {
+                                next = new MiddleOfTheLinkedList.ListNode(6)
+                            }
+                        }
+                    }
+                }
+            };
+
+            var expectedResult = new MiddleOfTheLinkedList.ListNode(4)
+            {
+                next = new MiddleOfTheLinkedList.ListNode(5)
+                {
+                    next = new MiddleOfTheLinkedList.ListNode(6)
+                }
+            };
+
+            var result = new MiddleOfTheLinkedList().Resolve(input);
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
