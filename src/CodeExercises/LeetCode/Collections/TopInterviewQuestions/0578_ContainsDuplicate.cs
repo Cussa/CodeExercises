1c864b0a45a9e9acd3c666859352d8731d503bdb
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-09 12:19:11 +0200

    feat: LeetCode TopInterviewQuestions - 578 - ContainsDuplicate

---
+++ src/CodeExercises/LeetCode/Collections/TopInterviewQuestions/0578_ContainsDuplicate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace CodeExercises.LeetCode.Collections.TopInterviewQuestions
+{
+    // https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/578/
+    public class ContainsDuplicate
+    {
+        public bool Resolve(int[] nums)
+        {
+            if (nums.Length < 2)
+                return false;
+
+            var set = new HashSet<int>();
+            foreach (var t in nums)
+            {
+                if (set.Contains(t))
+                    return true;
+
+                set.Add(t);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Collections/TopInterviewQuestions/0578_ContainsDuplicateTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeExercises.LeetCode.Collections.TopInterviewQuestions;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Collections.TopInterviewQuestions
+{
+    public class _0578_ContainsDuplicateTests
+    {
+        [Test]
+        [TestCase(new[] { 1, 2, 3, 1 }, true)]
+        [TestCase(new[] { 1, 2, 3, 4 }, false)]
+        [TestCase(new[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }, true)]
+        public void TestSolution(int[] input1, bool expectedResult)
+        {
+            var result = new ContainsDuplicate().Resolve(input1);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
