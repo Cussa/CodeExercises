711132aef1e4ac52099837cd9a835e664d717348
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-09 12:10:46 +0200

    feat: LeetCode 30DaysCoding Chanllenge - April 09 - BackspaceStringCompare

---
+++ src/CodeExercises/LeetCode/Chanllenges/ThirtyDayApril2020/09_BackspaceStringCompare.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    // 2020-04-09: https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/529/week-2/3291/
+    public class BackspaceStringCompare
+    {
+        public bool Resolve(string S, string T)
+        {
+            return RemoveBackspaces(S) == RemoveBackspaces(T);
+        }
+
+        private string RemoveBackspaces(string input)
+        {
+            var output = input;
+            var backspaceIndex = input.IndexOf('#');
+            while (backspaceIndex > -1)
+            {
+                var start = -1;
+                var remove = 2;
+                if (backspaceIndex == 0)
+                {
+                    start++;
+                    remove--;
+                }
+                output = output.Remove(backspaceIndex + start, remove);
+
+                backspaceIndex = output.IndexOf('#');
+            }
+
+            return output;
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Chanllenges/ThirtyDayApril2020/09_BackspaceStringCompareTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class _09_BackspaceStringCompareTests
+    {
+        [Test]
+        [TestCase("ab#c", "ad#c", true)]
+        [TestCase("ab##", "c#d#", true)]
+        [TestCase("a##c", "#a#c", true)]
+        [TestCase("a#c", "b", false)]
+        public void TestSolution(string input1, string input2, bool expectedResult)
+        {
+            var result = new BackspaceStringCompare().Resolve(input1, input2);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
