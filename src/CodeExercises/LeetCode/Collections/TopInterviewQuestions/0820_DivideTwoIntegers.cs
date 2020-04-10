811647dac39b7fdef57f1ed76cdf0af9b651efd4
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-10 14:26:51 +0200

    feat: LeetCode TopInterviewQuestions - 0820 - DivideTwoIntegers

---
+++ src/CodeExercises/LeetCode/Collections/TopInterviewQuestions/0820_DivideTwoIntegers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CodeExercises.LeetCode.Collections.TopInterviewQuestions
+{
+    // https://leetcode.com/explore/interview/card/top-interview-questions-medium/113/math/820/
+    public class DivideTwoIntegers
+    {
+        public int Resolve(int dividend, int divisor)
+        {
+            try
+            {
+                return dividend / divisor;
+            }
+            catch (OverflowException)
+            {
+                return int.MaxValue;
+            }
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Collections/TopInterviewQuestions/0820_DivideTwoIntegers.cs
@@ -0,0 +1,18 @@
+﻿using CodeExercises.LeetCode.Collections.TopInterviewQuestions;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Collections.TopInterviewQuestions
+{
+    public class _0820_DivideTwoIntegers
+    {
+        [Test]
+        [TestCase(10, 3, 3)]
+        [TestCase(7, -3, -2)]
+        [TestCase(-2147483648, -1, 2147483647)]
+        public void TestSolution(int input1, int input2, int expectedResult)
+        {
+            var result = new DivideTwoIntegers().Resolve(input1, input2);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
