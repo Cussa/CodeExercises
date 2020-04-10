9a6c3f1b911ae39143e261ed2079f6fbf6ddcbd8
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-10 14:08:54 +0200

    feat: LeetCode 30DaysCoding Chanllenge - April 10 - MinStack

---
+++ src/CodeExercises/LeetCode/Chanllenges/ThirtyDayApril2020/10_MinStack.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    // 2020-04-10: https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/529/week-2/3292/
+    public class MinStack
+    {
+        private Stack<int> stack = new Stack<int>();
+        private int numberMin = int.MaxValue;
+
+        /** initialize your data structure here. */
+        public MinStack()
+        {
+        }
+
+        public void Push(int x)
+        {
+            stack.Push(x);
+            if (x < numberMin)
+                numberMin = x;
+        }
+
+        public void Pop()
+        {
+            stack.Pop();
+            numberMin = stack.Count > 0 ? stack.Min() : int.MaxValue;
+        }
+
+        public int Top()
+        {
+            return stack.Peek();
+        }
+
+        public int GetMin()
+        {
+            return numberMin;
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Chanllenges/ThirtyDayApril2020/10_MinStackTests.cs
@@ -0,0 +1,63 @@
+﻿using CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020;
+using Microsoft.Win32.SafeHandles;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    
+    public class _10_MinStackTests
+    {
+        [Test]
+        public void TestSolution()
+        {
+            var minStack = new MinStack();
+            minStack.Push(-2);
+            minStack.Push(0);
+            minStack.Push(-3);
+            
+            Assert.AreEqual(-3, minStack.GetMin());
+
+            minStack.Pop();
+
+            Assert.AreEqual(0, minStack.Top());
+            Assert.AreEqual(-2, minStack.GetMin());
+        }
+
+        //["MinStack","push","push","push",
+        //"top","pop","getMin",
+        //"pop","getMin","pop",
+        //"push","top","getMin",
+        //"push","top","getMin",
+        //"pop","getMin"]
+        
+        //[[],[2147483646],[2147483646],[2147483647],[],[],[],[],[],[],[2147483647],[],[],[-2147483648],[],[],[],[]]
+
+        [Test]
+        public void TestSolution1()
+        {
+            var minStack = new MinStack();
+            minStack.Push(2147483646);
+            minStack.Push(2147483646);
+            minStack.Push(2147483647);
+
+            minStack.Top();
+            minStack.Pop();
+            minStack.GetMin();
+
+            minStack.Pop();
+            minStack.GetMin();
+            minStack.Pop();
+
+            minStack.Push(2147483647);
+            minStack.Top();
+            minStack.GetMin();
+
+            minStack.Push(-2147483648);
+            minStack.Top();
+            minStack.GetMin();
+
+            minStack.Pop();
+            minStack.GetMin();
+        }
+    }
+}
