9f36d88c277b9fe672c76f38cd6e57ec42c68a82
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-13 12:22:28 +0200

    feat: LeetCode 30DaysCoding Chanllenge - April 13 - ContiguousArray

---
+++ src/CodeExercises/LeetCode/Chanllenges/ThirtyDayApril2020/13_ContiguousArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class ContiguousArray
+    {
+        public int Resolve(int[] nums)
+        {
+            var maxArray = 0;
+            var count = 0;
+            var map = new Dictionary<int, int> { { 0, -1 } };
+            for (var i = 0; i < nums.Length; i++)
+            {
+                count += (nums[i] == 1 ? 1 : -1);
+
+                if (map.ContainsKey(count))
+                    maxArray = Math.Max(maxArray, i - map[count]);
+                else
+                    map.Add(count, i);
+            }
+
+            return maxArray;
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Chanllenges/ThirtyDayApril2020/13_ContiguousArrayTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class _13_ContigousArrayTests
+    {
+        [Test]
+        [TestCase(new[] { 0, 1, 0 }, 2)]
+        [TestCase(new[] { 0, 1 }, 2)]
+        [TestCase(new[] { 0, 0, 1, 1 }, 4)]
+        [TestCase(new[] { 0, 1, 1, 0, 1, 1, 1, 0 }, 4)]
+        public void TestSolution(int[] input1, int expectedResult)
+        {
+            var result = new ContiguousArray().Resolve(input1);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
