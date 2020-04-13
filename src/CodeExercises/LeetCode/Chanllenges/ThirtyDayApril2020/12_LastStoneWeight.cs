4fcb41924d9c7080b5d57dcd4f036345b798f6d3
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-13 11:42:04 +0200

    feat: LeetCode 30DaysCoding Chanllenge - April 12 - LastStoneWeight

---
+++ src/CodeExercises/LeetCode/Chanllenges/ThirtyDayApril2020/12_LastStoneWeight.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class LastStoneWeight
+    {
+        public int Resolve(int[] stones)
+        {
+            var listStones = stones.ToList();
+
+            while (listStones.Count > 1)
+            {
+                listStones = listStones.OrderByDescending(x => x).ToList();
+                var stone1 = listStones.Take(1).Single();
+                var stone2 = listStones.Skip(1).Take(1).Single();
+
+                listStones.RemoveRange(0, 2);
+
+                var newStone = stone1 - stone2;
+                if (newStone > 0)
+                    listStones.Add(newStone);
+            }
+
+            return listStones.Count == 1 ? listStones[0] : 0;
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Chanllenges/ThirtyDayApril2020/12_LastStoneWeightTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class _12_LastStoneWeightTests
+    {
+        [Test]
+        [TestCase(new[] { 2, 7, 4, 1, 8, 1 }, 1)]
+        //[TestCase(new[] { 1, 1, 3, 3, 5, 5, 7, 7 }, 0)]
+        //[TestCase(new[] { 1, 3, 2, 3, 5, 0 }, 3)]
+        //[TestCase(new[] { 1, 1, 2, 2 }, 2)]
+        public void TestSolution(int[] input1, int expectedResult)
+        {
+            var result = new LastStoneWeight().Resolve(input1);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
