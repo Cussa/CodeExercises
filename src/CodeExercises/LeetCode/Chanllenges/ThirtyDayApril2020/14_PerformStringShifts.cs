c8af0e519e770ff3e02d1a635780804e6634f312
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-16 09:44:57 +0200

    feat: LeetCode 30DaysCoding Chanllenge - April 14 - PerformStringShifts

---
+++ src/CodeExercises/LeetCode/Chanllenges/ThirtyDayApril2020/14_PerformStringShifts.cs
@@ -0,0 +1,30 @@
+﻿namespace CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class PerformStringShifts
+    {
+        public string Resolve(string s, int[][] shift)
+        {
+            if (shift == null || shift.Length == 0)
+                return s;
+
+            var moveLeft = 0;
+            foreach (var change in shift)
+            {
+                var direction = change[0];
+                var amount = change[1];
+
+                moveLeft += direction == 0 ? amount : -amount;
+            }
+
+            moveLeft %= s.Length;
+
+            if (moveLeft == 0)
+                return s;
+
+            if (moveLeft < 0)
+                moveLeft = s.Length + moveLeft;
+
+            return s.Substring(moveLeft) + s.Substring(0, moveLeft);
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Chanllenges/ThirtyDayApril2020/14_PerformStringShiftsTests.cs
@@ -0,0 +1,38 @@
+﻿using CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class _14_PerformStringShiftsTests
+    {
+        private static readonly object[] sourceObjects =
+        {
+            new object[]
+            {
+                "abc",
+                new[] { new[] {0,1}, new[] {1,2} },
+                "cab"
+            },
+            new object[]
+            {
+                "abcdefg",
+                new[] { new[] { 1, 1 }, new[] { 1, 1 }, new[] { 0, 2 }, new[] { 1, 3 } },
+                "efgabcd"
+            },
+            new object[]
+            {
+                "wpdhhcj",
+                new[] { new[] { 0, 7 }, new[] { 1, 7 }, new[] { 1, 0 }, new[] { 1, 3 }, new[] { 0, 3 }, new[] { 0, 6 }, new[] { 1, 2 } },
+                "hcjwpdh"
+            }
+        };
+
+        [Test]
+        [TestCaseSource(nameof(sourceObjects))]
+        public void TestSolution(string input1, int[][] input2, string expectedResult)
+        {
+            var result = new PerformStringShifts().Resolve(input1, input2);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
