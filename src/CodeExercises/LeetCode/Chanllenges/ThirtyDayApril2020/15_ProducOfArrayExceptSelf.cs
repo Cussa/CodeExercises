37375b558adce5ccf63e0163c04e42dc7695804e
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-16 09:45:16 +0200

    feat: LeetCode 30DaysCoding Chanllenge - April 15 - ProducOfArrayExceptSelf

---
+++ src/CodeExercises/LeetCode/Chanllenges/ThirtyDayApril2020/15_ProducOfArrayExceptSelf.cs
@@ -0,0 +1,25 @@
+﻿namespace CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class ProducOfArrayExceptSelf
+    {
+        public int[] Resolve(int[] nums)
+        {
+            var answer = new int[nums.Length];
+            answer[0] = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                answer[i] = nums[i - 1] * answer[i - 1];
+            }
+
+            var right = 1;
+            for (int i = nums.Length-1; i >= 0; i--)
+            {
+                answer[i] = answer[i] * right;
+                right *= nums[i];
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Chanllenges/ThirtyDayApril2020/15_ProducOfArrayExceptSelfTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class _15_ProducOfArrayExceptSelfTests
+    {
+        [Test]
+        [TestCase(new[] { 1, 2, 3, 4 }, new[] { 24, 12, 8, 6 })]
+        [TestCase(new[] { 4, 5, 1, 8, 2, 10, 6 }, new[] { 4800, 3840, 19200, 2400, 9600, 1920, 3200 })]
+        public void TestSolution(int[] input1, int[] expectedResult)
+        {
+            var result = new ProducOfArrayExceptSelf().Resolve(input1);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
