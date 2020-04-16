1bc505bdd27a79701ff7741dad183c294380c35b
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-16 11:16:20 +0200

    feat: LeetCode 30DaysCoding Chanllenge - April 16 - ValidParenthesisString

---
+++ src/CodeExercises/LeetCode/Chanllenges/ThirtyDayApril2020/16_ValidParenthesisString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class ValidParenthesisString
+    {
+        public bool Resolve(string s)
+        {
+            var lower = 0;
+            var higher = 0;
+            foreach (var letter in s)
+            {
+                lower += letter == '(' ? 1 : -1;
+                higher += letter != ')' ? 1 : -1;
+                if (higher < 0)
+                    break;
+
+                lower = Math.Max(lower, 0);
+            }
+
+            return lower == 0;
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Chanllenges/ThirtyDayApril2020/16_ValidParenthesisStringTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class _16_ValidParenthesisStringTests
+    {
+        [Test]
+        [TestCase("()", true)]
+        [TestCase("(*)", true)]
+        [TestCase("(*))", true)]
+        [TestCase(")", false)]
+        [TestCase("())", false)]
+        [TestCase("(*()", true)]
+        [TestCase(")(", false)]
+        public void TestSolution(string input1, bool expectedResult)
+        {
+            var result = new ValidParenthesisString().Resolve(input1);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
