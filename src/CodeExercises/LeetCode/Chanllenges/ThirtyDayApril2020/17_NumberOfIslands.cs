db0b2e9bdbc384a4f49d3392ded8bfca12079b0f
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-17 12:49:04 +0200

    feat: LeetCode 30DaysCoding Chanllenge - April 17 - NumberOfIslands

---
+++ src/CodeExercises/LeetCode/Chanllenges/ThirtyDayApril2020/17_NumberOfIslands.cs
@@ -0,0 +1,46 @@
+﻿namespace CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class NumberOfIslands
+    {
+        private const char Zero = '0';
+
+        public int Resolve(char[][] grid)
+        {
+            var numberOfIslands = 0;
+
+            for (var r = 0; r < grid.Length; r++)
+            {
+                for (var c = 0; c < grid[r].Length; c++)
+                {
+                    if (grid[r][c] == Zero)
+                        continue;
+
+                    Search(grid, r, c);
+                    numberOfIslands++;
+                }
+            }
+
+            return numberOfIslands;
+        }
+
+        private void Search(char[][] grid, int r, int c)
+        {
+            if (grid[r][c] == Zero)
+                return;
+
+            grid[r][c] = Zero;
+
+            if (r + 1 < grid.Length)
+                Search(grid, r + 1, c);
+
+            if (r - 1 >= 0)
+                Search(grid, r - 1, c);
+
+            if (c + 1 < grid[0].Length)
+                Search(grid, r, c + 1);
+
+            if (c - 1 >= 0)
+                Search(grid, r, c - 1);
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Chanllenges/ThirtyDayApril2020/17_NumberOfIslandsTests.cs
@@ -0,0 +1,46 @@
+﻿using CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class _17_NumberOfIslandsTests
+    {
+        private static readonly object[] SourceObjects =
+        {
+            new object[]
+            {
+                new[] { new[] { '1', '1', '1', '1', '0' },
+                        new[] { '1', '1', '0', '1', '0' },
+                        new[] { '1', '1', '0', '0', '0' },
+                        new[] { '0', '0', '0', '0', '0' }
+                },
+                1
+            },
+            new object[]
+            {
+                new[] { new[] { '1', '1', '0', '0', '0' },
+                        new[] { '1', '1', '0', '0', '0' },
+                        new[] { '0', '0', '1', '0', '0' },
+                        new[] { '0', '0', '0', '1', '1' }
+                },
+                3
+            },
+            new object[]
+            {
+                new[] { new[] { '1', '1', '1' },
+                        new[] { '0', '1', '0' },
+                        new[] { '1', '1', '1' }
+                },
+                1
+            }
+        };
+
+        [Test]
+        [TestCaseSource(nameof(SourceObjects))]
+        public void TestSolution(char[][] input1, int expectedResult)
+        {
+            var result = new NumberOfIslands().Resolve(input1);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
