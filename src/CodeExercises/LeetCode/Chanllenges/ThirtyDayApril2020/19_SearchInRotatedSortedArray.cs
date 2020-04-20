ccf91db41e984ceae83f3011efde2f5e46cafd5b
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-20 18:02:44 +0200

    feat: LeetCode 30DaysCoding Chanllenge - April 19 - SearchInRotatedSortedArray

---
+++ src/CodeExercises/LeetCode/Chanllenges/ThirtyDayApril2020/19_SearchInRotatedSortedArray.cs
@@ -0,0 +1,49 @@
+﻿namespace CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class SearchInRotatedSortedArray
+    {
+        public int Resolve(int[] nums, int target)
+        {
+            if (nums?.Length == 0)
+                return -1;
+
+            var left = 0;
+            var right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                var middle = (left + right) / 2;
+                var current = nums[middle];
+                var currentLeft = nums[left];
+                var currentRight = nums[right];
+
+                if (current == target)
+                    return middle;
+
+                if (current < currentLeft)
+                {
+                    if (target <= currentRight && target > current)
+                        left = middle + 1;
+                    else
+                        right = middle - 1;
+                }
+                else if (current > currentRight)
+                {
+                    if (target >= currentLeft && target < current)
+                        right = middle - 1;
+                    else
+                        left = middle + 1;
+                }
+                else
+                {
+                    if (target > current)
+                        left = middle + 1;
+                    else
+                        right = middle - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Chanllenges/ThirtyDayApril2020/19_SearchInRotatedSortedArrayTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class _19_SearchInRotatedSortedArrayTests
+    {
+        [Test]
+        [TestCase(new[] { 4, 5, 6, 7, 0, 1, 2 }, 0, 4)]
+        [TestCase(new[] { 4, 5, 6, 7, 0, 1, 2 }, 3, -1)]
+        public void TestSolution(int[] input1, int input2, int expectedResult)
+        {
+            var result = new SearchInRotatedSortedArray().Resolve(input1, input2);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
