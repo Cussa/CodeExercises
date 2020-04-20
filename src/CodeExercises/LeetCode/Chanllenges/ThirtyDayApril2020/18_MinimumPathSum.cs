ec870b577c4483792e470bf7e8f8e02181b68050
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-20 18:02:28 +0200

    feat: LeetCode 30DaysCoding Chanllenge - April 18 - MinimumPathSum

---
+++ src/CodeExercises/LeetCode/Chanllenges/ThirtyDayApril2020/18_MinimumPathSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class MinimumPathSum
+    {
+        public int Resolve(int[][] grid)
+        {
+            for (var r = 0; r < grid.Length; r++)
+            {
+                for (var c = 0; c < grid[r].Length; c++)
+                {
+                    if (c == 0 && r == 0)
+                        continue;
+
+                    if (r - 1 < 0)
+                        grid[r][c] = grid[r][c] + grid[r][c - 1];
+                    else if (c - 1 < 0)
+                        grid[r][c] = grid[r][c] + grid[r - 1][c];
+                    else
+                        grid[r][c] = grid[r][c] + Math.Min(grid[r][c - 1], grid[r - 1][c]);
+                }
+            }
+
+            return grid[^1][grid[0].Length - 1];
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Chanllenges/ThirtyDayApril2020/18_MinimumPathSumTests.cs
@@ -0,0 +1,28 @@
+﻿using CodeExercises.LeetCode.Chanllenges.ThirtyDayApril2020;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Chanllenges.ThirtyDayApril2020
+{
+    public class _18_MinimumPathSumTests
+    {
+        private static readonly object[] SourceObjects =
+        {
+            new object[]
+            {
+                new[] { new[] { 1, 3, 1 },
+                        new[] { 1, 5, 1 },
+                        new[] { 4, 2, 1 }
+                },
+                7
+            }
+        };
+
+        [Test]
+        [TestCaseSource(nameof(SourceObjects))]
+        public void TestSolution(int[][] input1, int expectedResult)
+        {
+            var result = new MinimumPathSum().Resolve(input1);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
