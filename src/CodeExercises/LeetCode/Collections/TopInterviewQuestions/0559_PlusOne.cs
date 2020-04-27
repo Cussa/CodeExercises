5ef75a0a4bce1312264b4ba458554d8020f851b0
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-27 19:14:09 +0200

    feat: LeetCode TopInterviewQuestions - 0559 - PlusOne

---
+++ src/CodeExercises/LeetCode/Collections/TopInterviewQuestions/0559_PlusOne.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace CodeExercises.LeetCode.Collections.TopInterviewQuestions
+{
+    public class PlusOne
+    {
+        public int[] Resolve(int[] digits)
+        {
+            if (digits == null || digits.Length == 0)
+                return null;
+
+            var digitsList = new Stack<int>();
+
+            var carry = 1;
+            for (var i = digits.Length - 1; i >= 0; i--)
+            {
+                var current = digits[i] + carry;
+
+                carry = current / 10;
+                current %= 10;
+
+                digitsList.Push(current);
+            }
+
+            if (carry > 0)
+                digitsList.Push(carry);
+
+            return digitsList.ToArray();
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Collections/TopInterviewQuestions/0559_PlusOneTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeExercises.LeetCode.Collections.TopInterviewQuestions;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Collections.TopInterviewQuestions
+{
+    public class _0559_PlusOneTests
+    {
+        [Test]
+        [TestCase(new[] { 9 }, new[] { 1, 0 })]
+        [TestCase(new[] { 9, 9 }, new[] { 1, 0, 0 })]
+        [TestCase(new[] { 1, 2, 3, 1 }, new[] { 1, 2, 3, 2 })]
+        [TestCase(new[] { 1, 2, 3, 9 }, new[] { 1, 2, 4, 0 })]
+        public void TestSolution(int[] input1, int[] expectedResult)
+        {
+            var result = new PlusOne().Resolve(input1);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
