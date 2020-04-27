a6fd233bd2cd93be8caa9c456ffcc18159a20c44
Cussa Mitre <cussa.mitre@gmail.com>
2020-04-27 18:48:29 +0200

    feat: LeetCode TopInterviewQuestions - 0885 - ImplementStrStr

---
+++ src/CodeExercises/LeetCode/Collections/TopInterviewQuestions/0885_ImplementStrStr.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CodeExercises.LeetCode.Collections.TopInterviewQuestions
+{
+    // https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/885/
+    public class ImplementStrStr
+    {
+        public int Resolve(string haystack, string needle)
+        {
+            return string.IsNullOrEmpty(needle) ? 0 : haystack.IndexOf(needle, StringComparison.InvariantCulture);
+        }
+    }
+}
---
+++ src/CodeExercisesTests/LeetCode/Collections/TopInterviewQuestions/0885_ImplementStrStrTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeExercises.LeetCode.Collections.TopInterviewQuestions;
+using NUnit.Framework;
+
+namespace CodeExercisesTests.LeetCode.Collections.TopInterviewQuestions
+{
+    public class _0885_ImplementStrStrTests
+    {
+        [Test]
+        [TestCase("hello", "ll", 2)]
+        [TestCase("aaaaa", "ba", -1)]
+        [TestCase("hello", "", 0)]
+        public void TestSolution(string input1, string input2, int expectedResult)
+        {
+            var result = new ImplementStrStr().Resolve(input1, input2);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
